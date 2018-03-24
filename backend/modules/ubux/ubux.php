f38b4c6001aad6683762c1ea1ffbb8c159baa85d
Sehat Maruli Tua Samosir <ifs316020@students.del.ac.id>
2018-03-24 14:07:43 +0700

    Init cis-lite

---
+++ backend/modules/ubux/ubux.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\ubux;
+
+/**
+ * ubux module definition class
+ */
+class ubux extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\modules\ubux\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
