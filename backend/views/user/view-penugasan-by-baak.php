0ee087c0929bd509dda6b171c2122c47e4bfc0f5
Fedrick Sulaiman Siagian <superiorsiagian@gmail.com>
2020-07-20 14:21:16 +0700

    form view by BAAK

--- backend/views/user/view-penugasan-by-baak.php
+++ backend/views/user/view-penugasan-by-baak.php
@@ -8,6 +8,8 @@
     use yii\db\Query;
     //use yii\console\widgets\Table;
     //$table = new Table();
+    $this->params['header'] = 'Penugasan Pengajaran';
+    $uiHelper=\Yii::$app->uiHelper;
 ?>
 <html>
     <head>
@@ -33,13 +35,24 @@
     </head>
     <body>
             <?php $form = ActiveForm::begin();?>
-                <div class="col-xs-9" >
+                <?= $uiHelper->renderContentSubHeader('Cari Penugasan', ['icon' => 'fa fa-search']);?>
+                <?=$uiHelper->renderLine(); ?>
+                <div style="margin:20px">
+                    <h3>Tahun Ajaran</h3>
                     <?= Html::dropDownList('selectedProdi','',$list_prodi,['class'=>'form-control','prompt' => 'Pilih Prodi'])?>
                 </div>
-                <div class="col-xs-3" style="margin-bottom:20px">
-                    <?= Html::submitButton('Ganti Prodi',['class'=>['btn btn-primary','custom-btn']]);?>
+                <div style="margin:20px">
+                    <h3>Prodi dan Jenjang</h3>
+                    <?= Html::dropDownList('selectedProdi','',$list_prodi,['class'=>'form-control','prompt' => 'Pilih Prodi'])?>
                 </div>
-                <h2 style="color:#3c8dbc"><b><?= $selectedProdi ?></b></h2>
+                <div style="margin:20px">
+                    <h3>Tahun Ajaran</h3>
+                    <?= Html::dropDownList('selectedProdi','',$list_prodi,['class'=>'form-control','prompt' => 'Pilih Prodi'])?>
+                </div>
+                <div style="margin:20px">
+                    <?= Html::submitButton('Cari',['class'=>['btn btn-primary','custom-btn']]);?>
+                </div>
+                <!-- <h2 style="color:#3c8dbc"><b><?= $selectedProdi ?></b></h2>
                 <table style="width:100%;" border="1">
                     <tr style="background : #f0f0f0">
                         <th rowspan="2">Kode Mata Kuliah</th>
@@ -87,56 +100,9 @@
                         <td><?= '100%' ?></td>
                     </tr>
                     <?php ++$i; }?>
-                </table>
+                </table> -->
 
                 <br>
             <?php $form = ActiveForm::end(); ?>
     </body>
 </html>
-<?php
-                    // $dataProvider = new ActiveDataProvider([
-                    //     'query' => KrkmKuliah::find(),
-                    //     'pagination' => [
-                    //         'pageSize' => 10
-                    //     ],
-
-                    // ]);
-
-                    // echo GridView::widget([
-                    //     'dataProvider' => $dataProvider,
-                    //     'columns' =>[
-                    //         [
-                    //             'attribute' => 'nama_kul_ind',
-                    //             'label' => 'Nama Mata Kuliah'
-                    //         ],
-                    //         'sks',
-                    //         [
-                    //             'attribute' => 'kode_mk',
-                    //             'label' => 'Kode Mata Kuliah'
-                    //         ],
-                    //         // [
-                    //         //     'attribute' => 'status',
-                    //         //     'value' => function($data,$key,$index,$column){
-                    //         //         if($data->status == -1){
-                    //         //             return 'menunggu';
-                    //         //         }
-                    //         //         else if($data->status == 0){
-                    //         //             return 'ditolak';
-                    //         //         }
-                    //         //         else if($data->status == 1){
-                    //         //             return 'diterima';
-                    //         //         }
-                    //         //     },
-                    //         // ],
-                    //         // [
-                    //              return Html::a('<span class="btn btn-success btn-sm"><b class="">Approve</b></span>',['Approve', 'id' => $model['id_request']], ['title' => 'Approve', 'id_request' => 'modal-btn-view']);
-                    //         // ],
-                    //         // [
-                    //         //     'attribute' => 'pengajar_id',
-                    //         //     'label' => 'Dosen I',
-                    //         //     'filter' => $form->field($model,'singkatan_prodi')->dropDownList($list_prodi)
-                    //         // ],
-                    //     ]
-                    // ]);
-                    
-                ?>
